e90ffd40bd43fe8667d86672776e356d170f4301
Geoff Fox <fox.geoffrey@gmail.com>
2023-03-06 16:53:26 -0800

    Asign and display some products

--- src/app/products/product.ts
+++ src/app/products/product.ts
@@ -1,7 +1,10 @@
 export interface IProduct {
+  productId: number,
   imageUrl: string,
   productName:string,
   productCode: string,
   price: number,
-  releaseDate: Date
+  releaseDate: string,
+  description: string,
+  starRating: number
 }
